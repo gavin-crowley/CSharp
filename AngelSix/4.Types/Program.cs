f890f17a2c97d6913e2be02a97c9bab41bf38e4f
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2021-12-13 20:34:11 +0000

    first commit

---
+++ AngelSix/4.Types/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _4.Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Integral Types, from smallest up (hold whole numbers)
+            sbyte mySByte;
+            byte myByte;
+            short myShort;
+            ushort myUShort;
+            int myInt;
+            uint myUInt;
+            long myLong;
+            ulong myULong;
+
+
+            // floats
+            float myFloat;
+            double myDouble;
+            decimal myDecimal;
+
+
+            bool myBool;
+            char myChar;
+            string myString;
+
+            myBool = false;
+            myBool = true;
+
+            myChar = 'a';
+            myString = "aaa";
+
+            DateTime myDateTime;
+            TimeSpan myTimeSpan;
+
+            // Options for storing data
+            //
+            // 1. Whole numbers
+            // 2. Floating point numbers
+            // 3. Truth values
+            // 4. Text (char/string)
+            //
+        }
+    }
+}
+
+// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/integral-numeric-types
---
+++ AngelSix/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; // using directive
+
+namespace HelloWorld // Namespace declaration
+{
+    internal class Program // Create class
+    {
+        static void Main(string[] args) // Create method
+        {
+            Console.WriteLine("Hello World!"); // Expression statement
+        }
+    }
+}
+
+
+// RULES
+
+// 1. using directives and statments end with ;
+// 2. Namespaces, classes, methods use { }
