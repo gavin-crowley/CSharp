854543f59f2bfb43d67813e32cfb1992e508bb13
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-01-24 20:33:14 +0000

    Jan 24

---
+++ ShadSluiter/beginning-c#/while_loop_examples/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+// do while
+int x = 10;
+do
+{
+    x--;
+    Console.WriteLine("X = " + x);
+} while (x > 0);
+
+
+
+// factorial example - while loop
+
+// While loop that never runs
+
+// Infinite while loop example
+
+
+
+
+// Secret Number
+//int secretNumber = 0;
+//Console.WriteLine("Please type an integer");
+
+//while (secretNumber < 100)
+//{
+//    secretNumber = int.Parse(Console.ReadLine());
+//    Console.WriteLine("You entered this number: " + secretNumber + ". " +
+//        "I will ask you again until you enter something greater than 100");
+//}
+//Console.WriteLine("All done with input example.");
+
+
+
+// Counter
+//int counter = 0;
+//while (counter < 30)
+//{
+//    counter = counter + 5;
+//    Console.WriteLine("Counter =  " + counter);
+//}
+
+//Console.WriteLine("All done with counting example");
