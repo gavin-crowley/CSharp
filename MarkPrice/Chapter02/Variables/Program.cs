2f8277b1c6c230d1764a7d7448bc286f264adfb6
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-01-25 19:41:03 +0000

    Jan 25

---
+++ MarkPrice/Chapter02/Variables/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Xml;
+
+object height = 1.88; // storing a double in an object
+object name = "Amir"; // storing a string in an object
+Console.WriteLine($"{name} is {height} metres tall.");
+
+//int length1 = name.Length; // gives compile error!
+int length2 = ((string)name).Length; // tell compiler it is a string
+Console.WriteLine($"{name} has {length2} characters.");
+
+//int population = 66_000_000; // 66 million in UK
+//double weight = 1.88; // in kilograms
+//decimal price = 4.99M; // in pounds sterling
+//string fruit = "Apples"; // strings use double-quotes
+//char letter = 'Z'; // chars use single-quotes
+//bool happy = true; // Booleans have value of true or false
+
+var population = 66_000_000; // 66 million in UK
+var weight = 1.88; // in kilograms
+var price = 4.99M; // in pounds sterling
+var fruit = "Apples"; // strings use double-quotes
+var letter = 'Z'; // chars use single-quotes
+var happy = true; // Booleans have value of true or false
+
+var xml1 = new XmlDocument();
+XmlDocument xml2 = new XmlDocument();
+
+var file1 = File.CreateText("something1.txt");
+StreamWriter file2 = File.CreateText("something2.txt");
+
+
+Console.WriteLine($"default(int) = {default(int)}");
+Console.WriteLine($"default(bool) = {default(bool)}");
+Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+Console.WriteLine($"default(string) = {default(string)}");
+
+int number = 13;
+Console.WriteLine($"number has been set to: {number}");
+number = default;
+Console.WriteLine($"number has been reset to its default: {number}");
+
+string[] names;
+names = new string[4];
+
+names[0] = "Kate";
+names[1] = "Jack";
+names[2] = "Rebecca";
+names[3] = "Tom";
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+string[] names2 = new[] { "Kate", "Jack", "Rebecca", "Tom" };
+
+for (int i = 0; i < names2.Length; i++)
+{
+    Console.WriteLine(names2[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MarkPrice/Chapter02/Vocabulary/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
+System.Data.DataSet ds;
+HttpClient client;
+
+//#error version
+//Console.WriteLine();
+//Console.WriteLine("Hello Ahmed");
+//Console.WriteLine("Temperature on {0:D} is {1}C.", DateTime.Today, 23.4);
+
+    Assembly? assembly = Assembly.GetEntryAssembly();
+    if (assembly == null) return;
+
+    foreach (AssemblyName name in assembly.GetReferencedAssemblies())
+    {
+    Assembly a = Assembly.Load(name);
+    int methodCount = 0;
+
+    foreach (TypeInfo t in a.DefinedTypes)
+    {
+        methodCount += t.GetMethods().Count();
+
+    }
+
+    Console.WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.",
+    arg0: a.DefinedTypes.Count(),
+    arg1: methodCount, arg2: name.Name);
+    }
+
