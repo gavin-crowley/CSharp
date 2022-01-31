ba51a1df886cc0d35191aaa8b7b3e45ca1f33a35
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-01-31 16:43:47 +0000

    Jan 31

--- BhrugenPatel/BulkyBook/BulkyBookWeb/Program.cs
+++ BhrugenPatel/BulkyBook/BulkyBookWeb/Program.cs
@@ -4,12 +4,11 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
