07180660d7a771893a8d3deb0330fc57fde4c633
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-02-01 14:03:29 +0000

    Feb 1 v1

---
+++ MarkPrice/Chapter05/PacktLibrary/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic; // List<T>
+
+namespace Packt.Shared // file-scoped namespace declaration
+{
+    public class Person : object
+    {
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public WondersOfTheAncientWorld FavoriteAncientWonder;
+        public WondersOfTheAncientWorld BucketList;
+        public List<Person> Children = new List<Person>();
+        public const string Species = "Homo Sapien";
+        public readonly string HomePlanet = "Earth";
+        public readonly DateTime Instantiated;
+
+        public Person()
+        {
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
+
+        public Person(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
+
+
+        // methods
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}");
+        }
+
+        public string GetOrigin()
+        {
+            return $"{Name} was born on {HomePlanet}";
+        }
+
+        public (string, int) GetFruit()
+        {
+            return ("Apples", 5);
+        }
+    }
+}
