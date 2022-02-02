d6ee9b9896ec70a119ea3e2c876211f272ff0a51
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-02-02 13:59:45 +0000

    Bhrugen basic crud finished

--- w3_csharp/BasicExercises/sandbox/Program.cs
+++ w3_csharp/BasicExercises/sandbox/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.WriteLine("Input integer:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input integer:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num1 > 0 ? true : false);
+Console.WriteLine(num2 > 0 ? true : false);
