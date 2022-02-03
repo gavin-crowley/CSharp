d8367fa2596215320ae80488c7d2ec0b931f9826
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-02-03 19:54:05 +0000

    Rob Miles added

---
+++ MSFT/RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        //defines a Price property that has a get and set method
+        //(getter and setter)
+        public decimal Price { get; set; }
+    }
+}
