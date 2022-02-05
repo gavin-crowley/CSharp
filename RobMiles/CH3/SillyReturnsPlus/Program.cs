16c7935b184e0480a0844c736c1735f5c2cea24f
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-02-05 17:30:58 +0000

    Feb 5

---
+++ RobMiles/CH3/SillyReturnsPlus/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ReturnDemo
+{
+    static int sillyReturnPlus(int i)
+    {
+        i = i + 1;
+        Console.WriteLine("i is : " + i);
+        return i;
+    }
+
+    static void Main()
+    {
+        int res;
+        res = sillyReturnPlus(5) + sillyReturnPlus(7) + 1;
+        Console.WriteLine("res is : " + res);
+    }
+}
