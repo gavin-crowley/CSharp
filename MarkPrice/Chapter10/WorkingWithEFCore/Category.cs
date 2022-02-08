6011f50950a1bc2d08883386aec57d70ea9f2629
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-02-08 15:53:16 +0000

    some ef core stuff in MP book

---
+++ MarkPrice/Chapter10/WorkingWithEFCore/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace   Packt.Shared
+{
+    // Category entity model class
+    public class Category
+    {
+        // these properties map to columns in the database
+        public int CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+
+        [Column(TypeName = "ntext")]
+        public string? Description { get; set; }
+
+
+        // defines a navigation property for related rows
+        public virtual ICollection<Product> Products { get; set; }
+
+        public Category()
+        {
+            // to enable developers to add products to a Category we must
+            // initialize the navigation property to an empty collection
+            Products = new HashSet<Product>();
+        }
+    }
+}
---
+++ MarkPrice/Chapter10/WorkingWithEFCore/bin/Debug/net6.0/Northwind.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packt.Shared
+{
+    public class Northwind : DbContext
+    {
+        // these properties map to tables in the database
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection = "Data Source=.;" +
+                "Initial Catalog=Northwind;" +
+                "Integrated Security=true;" +
+                "MultipleActiveResults=true;";
+
+            //set the database connection string
+            optionsBuilder.UseSqlServer(connection);
+        }
+
+        protected override void OnModelCreating(
+            ModelBuilder modelBuilder)
+        {
+            // example of using Fluent API instead of attributes
+            // to limit the length of a category name to 15
+
+            modelBuilder.Entity<Category>()
+                .Property(category => category.CategoryName)
+                .IsRequired() // NOT NULL
+                .HasMaxLength(15);
+        }
+    }
+}
