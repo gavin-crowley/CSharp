f2986e975414fd3e5a3a3228e4be9f1a09e19c7c
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-02-11 18:54:41 +0000

    commented

--- w3_csharp/BasicExercises/celsius/Program.cs
+++ w3_csharp/BasicExercises/celsius/Program.cs
@@ -4,6 +4,8 @@ int celsius = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Kelvin = {0}", Math.Round(celsius + 273.15), 2);
 Console.WriteLine("Fahrenheit = {0}", (celsius * 9 / 5) + 32);
 
+// comment 
+
 
 
 
