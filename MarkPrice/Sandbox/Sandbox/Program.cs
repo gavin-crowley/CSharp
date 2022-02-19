3990058e1fb632773735fc8d3d66dd98fc210ff2
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-02-19 19:19:54 +0000

    Feb 19

---
+++ MarkPrice/Sandbox/Sandbox/Program.cs
@@ -0,0 +1,35 @@
+﻿//using static System.Console;
+
+
+string str = "hey";
+int i = 6;
+
+if (i < 0)
+{
+    Console.WriteLine("hey");
+}
+else if (i > 0)
+{
+    Console.WriteLine("ho");
+}
+else
+{
+    Console.WriteLine("blah");
+}
+
+//switch (i)
+//{
+//    case 0:
+//        Console.WriteLine("jey");
+//        break;
+//    case 1:
+//        Console.WriteLine("gey");
+//        break;
+//    case 2:
+//        Console.WriteLine("hey");
+//        break;
+//    default:
+//        Console.WriteLine("default");
+//        break;
+//}
+
