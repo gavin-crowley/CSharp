842767b9d505499761cc63480c26fa06b150b874
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-02-20 16:50:38 +0000

    Feb 20

---
+++ MarkPrice/MyChapter05/MyPacktLibrary/Person.cs
@@ -0,0 +1,22 @@
+﻿using MyPacktLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPackt.Shared
+{
+    public class Person : object
+    {
+        public string Name;
+        public DateTime DateOfBirth;
+        public WondersOfTheAncientWorld FavoriteAncientWonder;
+        public WondersOfTheAncientWorld BucketList;
+
+        public List<Person> Children = new List<Person>();
+//        List<Person> is read aloud as "list of Person,"
+//        for example, "the type of the property named
+            //Children is a list of Person instances."
+
+
+    }
+}
---
+++ MarkPrice/MyChapter05/PeopleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using MyPackt.Shared;
+using MyPacktLibrary;
+using static System.Console;
+
+Person bob = new Person();
+//Console.WriteLine(bob.ToString());
+
+bob.Name = "Bob Smith";
+bob.DateOfBirth = new DateTime(1965, 12, 22); // C# 1.0 or later
+Console.WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}",
+arg0: bob.Name,
+arg1: bob.DateOfBirth);
+
+//Person alice = new()
+//{
+//    Name = "Alice Jones",
+//    DateOfBirth = new(1998, 3, 7) // C# 9.0 or later
+//};
+//WriteLine(format: "{0} was born on {1:dd MMM yy}",
+//arg0: alice.Name,
+//arg1: alice.DateOfBirth);
+
+//bob.FavoriteAncientWonder = WondersOfTheAncientWorld.
+//StatueOfZeusAtOlympia;
+//WriteLine(
+//format: "{0}'s favorite wonder is {1}. Its integer is {2}.",
+//arg0: bob.Name,
+//arg1: bob.FavoriteAncientWonder,
+//arg2: (int)bob.FavoriteAncientWonder);
+
+
+//bob.BucketList =
+//WondersOfTheAncientWorld.HangingGardensOfBabylon
+//| WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+//// bob.BucketList = (WondersOfTheAncientWorld)18;
+//WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
+
+//bob.Children.Add(new Person { Name = "Alfred" }); // C# 3.0 and later
+//bob.Children.Add(new() { Name = "Zoe" }); // C# 9.0 and later
+//WriteLine($"{bob.Name} has {bob.Children.Count} children:");
+//for (int childIndex = 0; childIndex < bob.Children.Count; childIndex++)
+//{
+//    WriteLine($" {bob.Children[childIndex].Name}");
+//}
+
+BankAccount.InterestRate = 0.012M; // store a shared value
+
+BankAccount jonesAccount = new(); // C# 9.0 and later
+jonesAccount.AccountName = "Mrs. Jones";
+jonesAccount.Balance = 2400;
+
+WriteLine(format: "{0} earned {1:C} interest.",
+arg0: jonesAccount.AccountName,
+arg1: jonesAccount.Balance * BankAccount.InterestRate);
+
+BankAccount gerrierAccount = new();
+gerrierAccount.AccountName = "Ms. Gerrier";
+gerrierAccount.Balance = 98;
+
+WriteLine(format: "{0} earned {1:C} interest.",
+arg0: gerrierAccount.AccountName,
+arg1: gerrierAccount.Balance * BankAccount.InterestRate);
