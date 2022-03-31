74d3b9ca8a665903ab9323d42ec45855456a8ce3
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-03-31 18:23:48 +0100

    pro asp march 31

---
+++ AFreeman/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿//using LanguageFeatures.Models;
+//using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            decimal cartTotal = cart.TotalPrices();
+            return View("Index", new string[] { $"Total: {cartTotal:C2}" });
+        }
+    }
+}
